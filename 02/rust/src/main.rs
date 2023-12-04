6b4c8fc1af78ebe3fc3af9a99e2fbc9343abe3a9
Jacob Latonis <jlatonis@me.com>
2023-12-03 21:41:26 -0600

    day 02 part 01 rust done

--- 02/rust/src/main.rs
+++ 02/rust/src/main.rs
@@ -5,45 +5,93 @@ use std::path::Path;
 
 fn main() {
     let input_path =
-        Path::new(env::current_dir().expect("Where am I?").parent().unwrap()).join("test-input");
+        Path::new(env::current_dir().expect("Where am I?").parent().unwrap()).join("input");
     let contents = fs::read_to_string(input_path).expect("Where did my file go?");
 
     part_one(&contents);
     // part_two(&contents);
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Unknown,
+}
+
+#[derive(Copy, Debug, Clone)]
 struct MarbleValue {
-    color: String,
+    color: Color,
     amount: u32,
+}
+
+#[derive(Debug, Clone)]
+struct MarbleGame {
+    marbles: Vec<MarbleValue>,
 }
 
 fn parse(line: &str) -> MarbleValue {
     let split_line = line.split(" ").collect::<Vec<_>>();
-    
+    let color = match split_line[1].parse::<String>().unwrap().as_str() {
+        "red" => Color::Red,
+        "green" => Color::Green,
+        "blue" => Color::Blue,
+        _ => Color::Unknown,
+    };
+
     MarbleValue {
-        color: split_line[1].parse::<String>().unwrap(),
-        amount: split_line[0].parse::<u32>().unwrap()
+        color,
+        amount: split_line[0].parse::<u32>().unwrap(),
     }
 }
 
 fn part_one(contents: &String) {
-    // let mut valid_ids_sum: u32 = 0;
-    let color_max = HashMap::from([("red", 12), ("blue", 14), ("green", 13)]);
+    let mut total = 0;
+    let color_max = HashMap::from([(Color::Red, 12), (Color::Blue, 14), (Color::Green, 13)]);
+    let mut game: HashMap<u32, MarbleGame> = HashMap::new();
 
     contents.split("\n").for_each(|line| {
-        let game_contents =  line.split(":").collect::<Vec<_>>();
-        let game_id = game_contents[0]
-            .split(" ")
-            .collect::<Vec<_>>()[1]
+        let game_contents = line.split(":").collect::<Vec<_>>();
+        let game_id = game_contents[0].split(" ").collect::<Vec<_>>()[1]
             .parse::<u32>()
             .unwrap();
 
-        game_contents[1].split(";").for_each(|game| {
-            dbg!(game.trim().split(",").collect::<Vec<_>>().iter().map(|item| parse(item.trim())).collect::<Vec<_>>());
-        });
-    // println!("Part One: {contents}");
+        let marble_games: Vec<_> = game_contents[1]
+            .split(";")
+            .map(|game| {
+                game.trim()
+                    .split(",")
+                    .collect::<Vec<_>>()
+                    .iter()
+                    .map(|item| parse(item.trim()))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        game.insert(
+            game_id,
+            MarbleGame {
+                marbles: marble_games.iter().cloned().flatten().collect(),
+            },
+        );
     });
+
+    total += game
+        .into_iter()
+        .filter(|(_, marble_game)| {
+            let mut valid = true;
+            for marble in &marble_game.marbles {
+                if marble.amount > *color_max.get(&marble.color).unwrap() {
+                    valid = false;
+                }
+            }
+            valid
+        })
+        .map(|(id, _)| id)
+        .fold(0, |sum, item| sum + item);
+
+    println!("{}", total);
 }
 
 fn part_two(contents: &String) {
