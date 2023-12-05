b0d43bb032f1e5a43d65c2880ca05994ee877f97
Jacob Latonis <jlatonis@me.com>
2023-12-05 11:13:53 -0600

    chore: fix new_day script, sed on linux operates diff than sed on mac for in place

--- new_day.sh
+++ new_day.sh
@@ -6,7 +6,7 @@ if [[ $1 =~ [0-9]{1,2} ]]; then
     echo "Directory for day $1 created!"
     echo "Changing to the newly created directory..."
     cd ./$1/
-    find . -type f -name "Cargo.toml" -exec sed -i.bu "s/day/day$1/g" {} +
+    find . -type f -name "Cargo.toml" -exec sed -i "s/day/day$1/g" {} +
  
 else 
     echo "Directory not created, invalid input!"
