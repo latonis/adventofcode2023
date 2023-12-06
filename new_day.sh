569a8aa23c22e2dbd4971b83ad18ba18b57dd8bf
Jacob Latonis <jlatonis@me.com>
2023-12-05 22:57:09 -0600

    config change for rust-analyzer

--- new_day.sh
+++ new_day.sh
@@ -5,6 +5,7 @@ if [[ $1 =~ [0-9]{1,2} ]]; then
     cp -r ./template/* $1/
     echo "Directory for day $1 created!"
     echo "Changing to the newly created directory..."
+    echo "{\"rust-analyzer.linkedProjects\": [\"$1/rust/Cargo.toml\"]}" > .vscode/settings.json
     cd ./$1/
     find . -type f -name "Cargo.toml" -exec sed -i "s/day/day$1/g" {} +
  
